cfabe144ac03378f15402c333787008ea8de9b1f
whdgdii <165139287+whdgdii@users.noreply.github.com>
2024-09-05 17:42:37 +0800

    Add files via upload

--- Boss.h
+++ Boss.h
@@ -3,44 +3,150 @@
 using namespace std;
 class Boss {
 public:
-	string name;
-	int number;
-	int health;
-	int defend;
-	int attack;
-	
-	
+	 string name;
+	 int number;
+	 int health;
+	 int defend;
+	 int attack;
+
+
 	int exercise;
 	int money;
 
-	Boss();
-	~Boss();
-	string getName();
+	void setName(string newName) {
+		name = newName;
+	}
+	string getName() {
+		return name;
+	}
+	void setNumber(int whatNumber) {
+		number = whatNumber;
+	}
+	int getNumber() {
+		return number;
+	}
 
-	int getnumber();
-	void setNumber(int number);
-	
-	int getHealth();
-	void setHealth(int hp);
-	 
-	int getDefend();
-	void setDefend(int defend);
+	int getHealth() {
+		return health;
+	}
+	void setHealth(int whatHealth) {
+		health = whatHealth;
+	}
 
-	int getattack();
-	void setAttack(int attack);
-	
-	
-	int getExercise();
-	int getMoney();
+	int getDefend() {
+		return defend;
+	}
+	void setDefend(int whatDefend) {
+		defend = whatDefend;
+	}
 
-	void showBoss();
-	
+	int getAttack() {
+		return attack;
+	}
+	void setAttack(int whatAttack) {
+		attack = whatAttack;
+	}
+	int getExercise() {
+		return exercise;
+	}
+	int getMoney() {
+		return money;
+	}
+
+	void showBoss() {
+		cout << endl;
+		cout << "Äã¼´½«ÃæÁÙµÄµÄÊÇ" << endl;
+		cout << "Ãû³Æ£º" << getName() << endl;
+		cout << "ÉúÃüÖµ£º" << getHealth() << endl;
+		cout << "·ÀÓùÁ¦£º" << getDefend() << endl;
+		cout << "¹¥»÷Á¦£º" << getAttack() << endl;
+		
+	}
+
+	Boss(int whatNumber) {
+		setNumber(whatNumber);
+		switch (whatNumber) {
+		case 1:
+			name = "¶éÂäÓÂÕß";
+			number = 1;
+			health = 300;
+			defend = 40;
+			attack = 40;
+			exercise = 150;
+			money = 50;
+			break;
+		case 2:
+			name = "Ä©ÈÕ»Æ»è½Ì»Ê";
+			number = 2;
+			health = 350;
+			defend = 30;
+			attack = 60;
+			exercise = 200;
+			money = 80;
+			break;
+		case 3:
+			name = "ÄõÈÕÑªÄ§";
+			number = 3;
+			health = 500;
+			defend = 100;
+			attack = 100;
+
+
+			exercise = 400;
+			money = 100;
+			break;
+		case 4:
+			name = "°µÈÕÁ¶ÓüÁú";
+			number = 4;
+			health = 700;
+			defend = 150;
+			attack = 150;
+
+
+			exercise = 600;
+			money = 200;
+			break;
+		case 5:
+			name = "Ğ¡¹Ö1ºÅ";
+			number = 5;
+			health = 100;
+			defend = 10;
+			attack = 10;
+			exercise = 50;
+			money = 10;
+			break;
+		case 6:
+			name = "Ğ¡¹Ö2ºÅ";
+			number = 6;
+			health = 100;
+			defend = 10;
+			attack = 10;
+			exercise = 50;
+			money = 10;
+			break;
+		case 7:
+			name = "Ğ¡¹Ö3ºÅ";
+			number = 7;
+			health = 100;
+			defend = 10;
+			attack = 10;
+			exercise = 50;
+			money = 10;
+			break;
+		case 8:
+			name = "Ğ¡¹Ö4ºÅ";
+			number = 8;
+			health = 100;
+			defend = 10;
+			attack = 10;
+			exercise = 50;
+			money = 10;
+			break;
+		default:
+			cout << "Invalid boss number." << endl;
+			break;
+
+		}
+	}
 };
-class bigBoss::public Boss {
-public:
-	bigBoss();
-	bigBoss(int number);
-	~bigBoss() {};
-};
-
 
